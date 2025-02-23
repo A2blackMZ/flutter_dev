a128a5dbdcb4f97720c8659b7600f8727407d144
A2blackMZ <samsdinealidou956@gmail.com>
2025-02-23 19:59:31 +0100

    Signup screen update

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -8,48 +8,92 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/zikflow-logo.png', width: 100, height: 100),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFB3C7E6), Color(0xFF104590)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Image.asset('assets/images/zikflow-logo.png', width: 100, height: 100),
             const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
+            _buildTextField(_usernameController, "Nom d'utilisateur", Icons.person),
+                const SizedBox(height: 15),
+            _buildTextField(_emailController, 'Email', Icons.email),
             const SizedBox(height: 15),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Mot de passe',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
+            _buildTextField(_passwordController, 'Mot de passe', Icons.lock, obscureText: true),
+            const SizedBox(height: 15),
+            _buildTextField(_confirmPasswordController, 'Confirmer le mot de passe', Icons.lock, obscureText: true),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Checkbox(
+                  value: _isChecked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _isChecked = value!;
+                    });
+                  },
+                ),
+                const Text(
+                  "I agree to the terms and conditions",
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF104590),
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-              ),
-              onPressed: () {
-                // Action d'inscription
-              },
-              child: const Text('S\'inscrire', style: TextStyle(color: Colors.white)),
-            ),
-          ],
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: _isChecked ? () {} : null,
+                child: const Text(
+                    "S'inscrire",
+                style: TextStyle(color: Color(0xFF104590), fontWeight: FontWeight.bold),
+          ),
+        ),
+        ],
+      ),
+    ),
+    ),
+    ),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label, IconData icon, {bool obscureText = false}) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.white),
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.white70),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.2),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
         ),
       ),
     );
